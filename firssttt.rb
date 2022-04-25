a8a80b1100aa96d725af2e8e10a62bba1d99d5bf
Lefteris Giannaros <lefterisgiannaros@gmail.com>
2022-04-26 01:58:44 +0300

    Lead synth and rythm
    Added lead synth and basic rythm

---
+++ firssttt.rb
@@ -0,0 +1,29 @@
+live_loop :sawyou do
+  
+  use_synth :tech_saws
+  play 50, attack: 0.8
+  sleep 1
+  play 65, attack: 0.8
+  sleep 1
+  play 48, attack: 0.8
+  sleep 1
+  play 64, attack: 0.8
+  sleep 1
+  play 46, attack: 0.8
+  sleep 1
+  play 65, attack: 0.8
+  sleep 1
+  play 48, attack: 0.8
+  sleep 1
+  play 64, attack: 0.8
+  sleep 1
+  
+end
+
+live_loop :kickme do
+  sample "E:/[Music Production]/DAW Tools/Samples/The Weeknd Drum Kit (Sounds Used By The Weeknd) (@xephyrisdead)/Kicks/Angel Kick.wav"
+  play a
+end
+
+
+
